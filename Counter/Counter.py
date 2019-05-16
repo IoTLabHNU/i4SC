f211b4568e7fb57b56d04a58723fd8f6b6c095ab
IoTLabHNU <IoT.Lab.HNU@gmail.com>
2019-05-16 12:08:22 +0200

    counter system added

---
+++ Counter/Counter.py
@@ -0,0 +1,66 @@
+"""
+This Python Script uses a light emmitter (laser) and a light detector sensor [Dual Comperator and laser emmitter modules HW-483]
+"""
+import RPi.GPIO as GPIO
+import time
+
+alert_led = 18
+light_sensor = 4
+counter=0
+
+def setup():
+        """
+        This function sets up the GPIO pins to BCM mode and sets GPIO pin 4 for input GPIO pin 18.
+        :return: nothing
+        """
+        GPIO.setmode(GPIO.BCM)                  # Numbers GPIOs by BCM pinout
+        GPIO.setup(light_sensor,GPIO.IN)        # Set light_sensor pin mode as input
+        GPIO.setup(alert_led,GPIO.OUT)
+
+def loop():
+        """
+        This function counts the number of items/objects that pass past the laser light.
+        :return: number of items that passed past the laser
+        """ 
+        prev = False
+        current = False
+        global counter
+        
+        while True:
+                prev = current
+                output = bool(GPIO.input(light_sensor))
+                
+                current = output
+
+                if ((current==True) and (prev ==False)):        #if values switch between True and a False, one object has passed
+                        counter=counter+1
+                        print ('Counting in Progress '+ str(counter))
+                
+                if output:
+                        GPIO.output(alert_led,GPIO.HIGH)       #switch alert led ON      
+                else:
+                        GPIO.output(alert_led,GPIO.LOW)
+
+        return (counter)
+
+def resetCounter():
+        """
+        This function resets the counter to zero.
+        :return: nothing
+        """ 
+        counter = 0
+        
+def destroy():
+         """
+        This function clears up the GPIO pins
+        :return: nothing
+        """
+         GPIO.cleanup()                     # Release resources
+
+if __name__ == '__main__':     # Program start from here
+        setup()
+        try:
+                loop()
+        except KeyboardInterrupt:  # When 'Ctrl+C' is pressed, the child program destroy() will be  executed.
+                destroy()
+                print('Total is :'+ str(counter))
