ed41374c70170af490af5219e52d06b2b14dd62c
Kloniphani Maluleke <kloniphani@gmail.com>
2019-05-16 13:02:55 +0200

    Updating Sorting files

--- RunSortingSys.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/pyinstaller3
-from SortingSystem import sortingSys
-
-try:
-    SortingSys = sortingSys()    
-    SortingSys.runFunc()
-except Exception as l:
-    print("User pressed c...")
